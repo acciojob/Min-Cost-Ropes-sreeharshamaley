1eda8759b9f3261503696bacd6a056c0f9e72825
Sree Harsha <102215646+sreeharshamaley@users.noreply.github.com>
2025-06-17 14:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,83 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(val) {
+    this.heap.push(val);
+    this._bubbleUp();
+  }
+
+  extractMin() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._bubbleDown();
+    return min;
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  _bubbleUp() {
+    let index = this.heap.length - 1;
+    const current = this.heap[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.heap[parentIndex];
+      if (parent <= current) break;
+      this.heap[parentIndex] = current;
+      this.heap[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  _bubbleDown() {
+    let index = 0;
+    const length = this.heap.length;
+    const current = this.heap[0];
+    while (true) {
+      let leftIdx = 2 * index + 1;
+      let rightIdx = 2 * index + 2;
+      let smallest = index;
+
+      if (leftIdx < length && this.heap[leftIdx] < this.heap[smallest]) {
+        smallest = leftIdx;
+      }
+
+      if (rightIdx < length && this.heap[rightIdx] < this.heap[smallest]) {
+        smallest = rightIdx;
+      }
+
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+      index = smallest;
+    }
+  }
+}
+
+function mincost(arr) {
+  const heap = new MinHeap();
+  for (let num of arr) {
+    heap.insert(num);
+  }
+
+  let totalCost = 0;
+
+  while (heap.size() > 1) {
+    let first = heap.extractMin();
+    let second = heap.extractMin();
+    let cost = first + second;
+    totalCost += cost;
+    heap.insert(cost);
+  }
+
+  return totalCost;
+}
 }
 
 module.exports=mincost;
